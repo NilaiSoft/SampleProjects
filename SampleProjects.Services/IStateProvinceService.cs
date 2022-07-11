cf9f49426ab6ee76f9199e76644b19de831a8f6c
Ehsan Nozari <e.nozari@tikakala.com>
2022-07-11 12:44:10 +0430

    new version

---
+++ SampleProjects.Services/IStateProvinceService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SampleProjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProjects.Services
+{
+    public interface IStateProvinceService
+    {
+        Task<EntityEntry<StateProvince>> AddAsync(StateProvince item);
+        Task<int> AddAndSaveChangesAsync(StateProvince entity);
+        Task<int> EditAsync(Expression<Func<StateProvince, bool>> predicate,
+            Expression<Func<StateProvince, StateProvince>> expression);
+        Task<int> SaveChangesAsync();
+        Task<int> DeleteAsync(Expression<Func<StateProvince, bool>> _pridicate);
+        Task<IList<StateProvince>> GetsAsync(Expression<Func<StateProvince, bool>> _pridicate);
+        Task<IList<StateProvince>> GetsAsync();
+        Task<IList<StateProvince>> GetsAsync(Expression<Func<StateProvince, StateProvince>> expression);
+        Task<StateProvince> GetAsync(Expression<Func<StateProvince, bool>> _pridicate);
+        Task<StateProvince> GetAsync
+            (Expression<Func<StateProvince, bool>> _pridicate, Expression<Func<StateProvince, StateProvince>> selectItem);
+        Task<StateProvince> FindAsync(Expression<Func<StateProvince, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<StateProvince, bool>> expression);
+        Task<bool> AnyAsync();
+    }
+}
---
+++ SampleProjects.Services/StateProvinceService.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SampleProjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProjects.Services
+{
+    public class StateProvinceService : IStateProvinceService
+    {
+        private readonly IRepository<StateProvince> _StateProvinceRepository;
+
+        public StateProvinceService(IRepository<StateProvince> StateProvinceRepository)
+        {
+            _StateProvinceRepository = StateProvinceRepository;
+        }
+
+        public async Task<int> AddAndSaveChangesAsync(StateProvince entity)
+        {
+            return await _StateProvinceRepository.AddAndSaveChangesAsync(entity);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _StateProvinceRepository.SaveChangesAsync();
+        }
+
+        public async Task<EntityEntry<StateProvince>> AddAsync(StateProvince item)
+        {
+            return await _StateProvinceRepository.AddAsync(item);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<StateProvince, bool>> expression)
+        {
+            return await _StateProvinceRepository.AnyAsync(expression);
+        }
+
+        public async Task<bool> AnyAsync()
+        {
+            return await _StateProvinceRepository.AnyAsync();
+        }
+
+        public async Task<int> DeleteAsync(Expression<Func<StateProvince, bool>> _pridicate)
+        {
+            return await _StateProvinceRepository.DeleteAsync(_pridicate);
+        }
+
+        public async Task<StateProvince> FindAsync(Expression<Func<StateProvince, bool>> predicate)
+        {
+            return await _StateProvinceRepository.FindAsync(predicate);
+        }
+
+        public async Task<StateProvince> GetAsync(Expression<Func<StateProvince, bool>> _pridicate)
+        {
+            return await _StateProvinceRepository.GetAsync(_pridicate);
+        }
+
+        public async Task<IList<StateProvince>> GetsAsync(Expression<Func<StateProvince, bool>> _pridicate)
+        {
+            return await _StateProvinceRepository.GetsAsync(_pridicate);
+        }
+
+        public async Task<IList<StateProvince>> GetsAsync()
+        {
+            return await _StateProvinceRepository.GetsAsync(x => new StateProvince
+            {
+                Name = x.Name,
+                Id = x.Id
+            });
+        }
+
+        public async Task<IList<StateProvince>> GetsAsync(Expression<Func<StateProvince, StateProvince>> expression)
+        {
+            return await _StateProvinceRepository.GetsAsync();
+        }
+
+        public async Task<int> EditAsync
+            (Expression<Func<StateProvince, bool>> predicate, Expression<Func<StateProvince, StateProvince>> expression)
+        {
+            return await _StateProvinceRepository.EditAsync(predicate, expression);
+        }
+
+        public async Task<StateProvince> GetAsync(Expression<Func<StateProvince, bool>> _pridicate, Expression<Func<StateProvince, StateProvince>> selectItem)
+        {
+            return await _StateProvinceRepository.GetAsync(_pridicate, selectItem);
+        }
+    }
+}
