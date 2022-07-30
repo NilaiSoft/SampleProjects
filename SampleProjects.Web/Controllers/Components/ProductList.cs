088afca4b3531af1a9cb697632dbe93ea7b73cc2
Ehsan Nozari <e.nozari@tikakala.com>
2022-07-30 12:57:41 +0430

    store panel

---
+++ SampleProjects.Web/Controllers/Components/ProductList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NilaiSofts.Web.Controllers.Components
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
